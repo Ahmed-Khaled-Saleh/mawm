FIX: cumulative size calculation in BufferAwareConcatDataset

MAWM/data/loaders.py
@@ -125,7 +125,7 @@ class RolloutObservationDataset(_RolloutDataset): # pylint: disable=too-few-publ
         print(f"Getting data at index {seq_index}")
         done = data[f'{self.agent}_info'][seq_index]['done']
         obs = data[f'{self.agent}_obs'][seq_index][self.obs_key].astype(np.uint8)
-        return self._transform(obs), done
+        return self._transform(obs), done, self.agent
 
 
 # %% ../../nbs/01c_data_loaders.ipynb 28

MAWM/trainers/vae_trainer.py
@@ -63,7 +63,7 @@ def train_epoch(self: VAETrainer, epoch):
     
     for batch_idx, data in enumerate(self.train_loader):
         # import pdb; pdb.set_trace()
-        obs, dones = data
+        obs, dones, agent_id = data
         mask = ~dones.bool()     # keep only where done is False
 
         if mask.sum() == 0:
@@ -97,7 +97,7 @@ def eval_epoch(self: VAETrainer):
     test_loss = 0
     with torch.no_grad():
         for data in self.val_loader:
-            obs, dones = data
+            obs, dones, agent_id = data
             mask = ~dones.bool()     # keep only where done is False
 
             if mask.sum() == 0:

mains/main_vae.py
@@ -123,10 +123,10 @@ def main(cfg):
     dataset_test = BufferAwareConcatDataset(datasets= dss_test)
 
     train_loader = torch.utils.data.DataLoader(
-        dataset_train, batch_size=cfg.data.batch_size, shuffle=True, num_workers=2)
+        dataset_train, batch_size=cfg.data.batch_size, shuffle=True, num_workers=0)
 
     val_loader = torch.utils.data.DataLoader(
-        dataset_test, batch_size=cfg.data.batch_size, shuffle=True, num_workers=2)
+        dataset_test, batch_size=cfg.data.batch_size, shuffle=True, num_workers=0)
 
     
 