ADD: fixed losses calculation.

mains/vicreg_main.py
@@ -1,56 +1,34 @@
+
+import argparse
+from os.path import join
+import os
+import time
+from functools import reduce
+
+from omegaconf import OmegaConf
+from dotenv import load_dotenv
+from tqdm.auto import tqdm
 from fastcore import *
 from fastcore.utils import *
 
 
-import argparse
-from os.path import join, exists
-from os import mkdir
-import time
-import os
-from torch import optim
-from torch.nn import functional as F
-from torchvision import transforms
-from torch.utils.data import ConcatDataset
-from omegaconf import OmegaConf
-from dotenv import load_dotenv
-
-
-
-from mawm.core import get_cls, PRIMITIVE_TEMPLATES
-
-from mawm.data.utils import lejepa_train_tf, lejepa_test_tf
-from mawm.data.loaders import MarlGridDataset
-
-from mawm.trainers.trainer_progjepa import ProgLejepaTrainer
-from mawm.writers.wandb_writer import WandbWriter
-
-
-import warnings
-
-warnings.filterwarnings("ignore", message="Ill-formed record")
-
-from typing import Optional
-import os
-import shutil
-from dataclasses import dataclass
-import dataclasses
-import random
-import time
-
 import torch
 import numpy as np
-from tqdm.auto import tqdm
 from matplotlib import pyplot as plt
-import matplotlib
-from mawm.logger import Logger, MetricTracker
 
-from mawm.optimizers.schedulers import Scheduler, LRSchedule
+from mawm.logger import Logger, MetricTracker
+from mawm.core import get_cls
+from mawm.writers.wandb_writer import WandbWriter
+from mawm.data.utils import init_data
+from mawm.models.jepa import JEPA
+from mawm.models.utils import Projector
+from mawm.models.vision import SemanticEncoder
+from mawm.optimizers.schedulers import Scheduler
 from mawm.optimizers.factory import OptimizerFactory, OptimizerType
 from mawm.trainers.trainer_dynamics import DynamicsTrainer
 
-from mawm.models.jepa import JEPA
 
-parser = argparse.ArgumentParser(description='VAE Training')
+parser = argparse.ArgumentParser(description='Dynamics Training')
 parser.add_argument('--config', type=str, help='Path to the YAML config file', required=True)
 parser.add_argument('--timestamp', type=str, help='Time stamp', required=True)
 parser.add_argument('--env_file', type=str, help='Path to the .env file', required=False)
@@ -77,58 +55,43 @@ except:
 cfg.now = args.timestamp 
 
 
-def init_data(data_dir, batch_size, train=True):
-    train_ds = MarlGridDataset(root=data_dir,
-                               transform=lejepa_train_tf,
-                               seq_len=cfg.data.seq_len,
-                               train=train
-                               )
-        
-    test_ds = MarlGridDataset(data_path= data_dir, 
-                              transform= lejepa_test_tf,
-                              seq_len= cfg.data.seq_len,
-                              train= not train
-                              )
-        
-    train_loader = torch.utils.data.DataLoader(
-        train_ds,
-        batch_size=batch_size,
-        num_workers=0,
-        pin_memory=True,
-        drop_last=True,
-    )
-
-    val_loader = torch.utils.data.DataLoader(
-        test_ds,
-        batch_size=batch_size,
-        num_workers=0,
-        pin_memory=True,
-        drop_last=False,
-    )
-
-    return train_loader, val_loader 
-
-def init_opt(model):
+def init_opt(params):
         optimizer_cls = get_cls("torch.optim", cfg.optimizer.name)
-        optimizer = optimizer_cls(model.parameters(), lr=cfg.optimizer.lr)
+        optimizer = optimizer_cls(params, lr=cfg.optimizer.lr)
         return optimizer
 
 def main(cfg):
-    cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if cuda else "cpu")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     train_loader, val_loader = init_data(cfg.data.data_dir, cfg.data.batch_size, train= True)    
 
-    model = JEPA(cfg.model, action_dim= 1)
-    model = model.to(device)
-    optimizer = init_opt(model)
+    jepa = JEPA(cfg.model, action_dim= 1)
+    msg_encoder = SemanticEncoder(latent_dim = cfg.model.msg_encoder.latent_dim)
 
+    z_input_dim = reduce(lambda x, y: x * y, jepa.backbone.repr_dim)
+    c_input_dim = msg_encoder.latent_dim * cfg.data.seq_len
+    projector = Projector(z_input_dim= z_input_dim, c_input_dim=c_input_dim)
+    
+    model = torch.nn.ModuleDict({
+         'jepa': jepa,
+         'msg_encoder': msg_encoder,
+         'projector': projector
+    }).to(device)
+
+    all_params = (
+        list(jepa.parameters()) + 
+        list(msg_encoder.parameters()) + 
+        list(projector.parameters())
+    )
+
+    optimizer = init_opt(all_params)
     # TODO: Check this
     scheduler = Scheduler(optimizer, 'min', factor=0.5, patience=5) 
     
     # now = time.strftime("%Y%m%d-%H%M%S")
     # cfg.now = now
     writer = WandbWriter(cfg)
-    trainer = DynamicsTrainer(cfg, model, train_loader, val_loader, criterion= None, optimizer= optimizer,
+    trainer = DynamicsTrainer(cfg, model, train_loader, val_loader, optimizer= optimizer,
                             device= device, earlystopping= None, scheduler= scheduler, writer= writer)
 
     df_res = trainer.fit()

mawm/data/loaders.py
@@ -79,7 +79,7 @@ class MarlGridDataset(torch.utils.data.Dataset):
         
     
 
-# %% ../../nbs/01c_data.loaders.ipynb 19
+# %% ../../nbs/01c_data.loaders.ipynb 26
 from bisect import bisect
 from os import listdir
 from os.path import join, isdir
@@ -89,7 +89,7 @@ import torch.utils.data
 import numpy as np
 
 
-# %% ../../nbs/01c_data.loaders.ipynb 20
+# %% ../../nbs/01c_data.loaders.ipynb 27
 class _RolloutDataset(torch.utils.data.Dataset): 
     def __init__(self, agent, root, transform, buffer_size=200, seq_len= 50, train=True, obs_key = 'pov'): # pylint: disable=too-many-arguments
         
@@ -179,7 +179,7 @@ class _RolloutDataset(torch.utils.data.Dataset):
     def _data_per_sequence(self, data_length):
         raise NotImplementedError
 
-# %% ../../nbs/01c_data.loaders.ipynb 22
+# %% ../../nbs/01c_data.loaders.ipynb 29
 class RolloutObservationDataset(_RolloutDataset): # pylint: disable=too-few-public-methods
 
     def _data_per_sequence(self, data_length):
@@ -192,7 +192,7 @@ class RolloutObservationDataset(_RolloutDataset): # pylint: disable=too-few-publ
         return self._transform(obs), act, done, self.agent
 
 
-# %% ../../nbs/01c_data.loaders.ipynb 47
+# %% ../../nbs/01c_data.loaders.ipynb 54
 class RolloutSequenceDataset(_RolloutDataset): # pylint: disable=too-few-public-methods
     """ Encapsulates rollouts.
 
@@ -279,7 +279,7 @@ class RolloutSequenceDataset(_RolloutDataset): # pylint: disable=too-few-public-
         return data_length - self._seq_len
 
 
-# %% ../../nbs/01c_data.loaders.ipynb 57
+# %% ../../nbs/01c_data.loaders.ipynb 64
 import torch
 class LejepaVisionDataset(torch.utils.data.Dataset):
     def __init__(self, dataset, transform, V=1):
@@ -295,7 +295,7 @@ class LejepaVisionDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.ds)
 
-# %% ../../nbs/01c_data.loaders.ipynb 61
+# %% ../../nbs/01c_data.loaders.ipynb 68
 from datasets import load_from_disk, concatenate_datasets
 import torch
 class D4RL(torch.utils.data.Dataset): 

mawm/losses/vicreg.py
@@ -22,7 +22,7 @@ from ..models.utils import flatten_conv_output
 from functools import reduce
 import operator
 from ..models.misc import Projector
-
+from einops import rearrange
 
 
 # %% ../../nbs/03b_losses.vicreg.ipynb 6
@@ -48,21 +48,25 @@ class VICReg(torch.nn.Module):
 
     
 
-# %% ../../nbs/03b_losses.vicreg.ipynb 7
+# %% ../../nbs/03b_losses.vicreg.ipynb 9
 @patch
-def __call__(self: VICReg, encodings, state_predictions):
+def __call__(self: VICReg, encodings, state_predictions, mask= None):
     
-    sim_loss = (encodings[1:] - state_predictions[1:]).pow(2).mean()
+    valid_mask = rearrange(mask, 'b t -> t b')
+    transition_mask = valid_mask[1:] * valid_mask[:-1]# (T-1, B)
+    
+    diff = (encodings[1:] - state_predictions[1:]).pow(2).mean(dim=(2, 3, 4)) # (T-1, B)
+    sim_loss = (diff * transition_mask).sum() / transition_mask.sum().clamp_min(1)
 
     if self.cfg.loss.vicreg.sim_coeff_t:
-        sim_loss_t = (encodings[1:] - encodings[:-1]).pow(2).mean()
+        diff_t = (encodings[1:] - encodings[:-1]).pow(2).mean(dim=(2, 3, 4))# (T-1, B)
+        sim_loss_t = (diff_t * transition_mask).sum() / transition_mask.sum().clamp_min(1)
     else:
         sim_loss_t = torch.zeros([1])
 
     encodings = self.projector(encodings)
 
-    flat_encodings = flatten_conv_output(encodings)
-
+    flat_encodings = flatten_conv_output(encodings) # [T, B, D]
     std_loss = self.std_loss(flat_encodings[:1])
 
     if self.cfg.loss.vicreg.cov_per_feature:
@@ -76,12 +80,29 @@ def __call__(self: VICReg, encodings, state_predictions):
         # reshape (1, bs, ch, h, w) --> (w, bs, ch * h * w)
         cov_loss = self.cov_loss(flat_encodings[:1])
 
-    std_loss_t = self.std_loss(
-        flat_encodings[1:].permute(1, 0, 2), across_time=True
-    )  # (bs, T, repr)
-    cov_loss_t = self.cov_loss(
-        flat_encodings[1:].permute(1, 0, 2), across_time=True
-    )  # (bs, T, repr)
+    # flat_encodings: (T, B, D)
+    flat_enc = flat_encodings[1:]          # drop t=0
+    valid = valid_mask[1:]                 # (T-1, B)
+
+    # reshape to (B, T-1, D)
+    flat_enc = flat_enc.permute(1, 0, 2)
+    valid = valid.permute(1, 0)
+
+
+    std_losses, cov_losses = [], []
+
+    for b in range(flat_enc.shape[0]):
+        idx = valid[b].bool()
+        if idx.sum() > 1:   # must have at least 2 steps
+            x = flat_enc[b, idx]   # (T_valid, D)
+            std_losses.append(self.std_loss(x.unsqueeze(0), across_time=True))
+            cov_losses.append(self.cov_loss(x.unsqueeze(0), across_time=True))
+
+    if len(std_losses) > 0:
+        std_loss_t = torch.stack(std_losses).mean()
+        cov_loss_t = torch.stack(cov_losses).mean()
+    else:
+        std_loss_t = cov_loss_t = torch.zeros(1, device=flat_enc.device)
 
     total_loss = (
         self.cfg.loss.vicreg.sim_coeff * sim_loss
@@ -105,7 +126,7 @@ def __call__(self: VICReg, encodings, state_predictions):
     
     return losses
 
-# %% ../../nbs/03b_losses.vicreg.ipynb 8
+# %% ../../nbs/03b_losses.vicreg.ipynb 10
 @patch
 def std_loss(self:VICReg, x: torch.Tensor, across_time=False):
     x = x - x.mean(dim=1, keepdim=True)  # mean for each dim across batch samples
@@ -127,7 +148,7 @@ def std_loss(self:VICReg, x: torch.Tensor, across_time=False):
 
     return std_loss
 
-# %% ../../nbs/03b_losses.vicreg.ipynb 9
+# %% ../../nbs/03b_losses.vicreg.ipynb 11
 @patch
 def cov_loss(self: VICReg, x: torch.Tensor, across_time=False):
     batch_size = x.shape[1]

mawm/models/jepa.py
@@ -67,11 +67,11 @@ class JEPA(nn.Module):
 @patch
 def forward(
         self: JEPA,
-        x: torch.Tensor,
-        pos: torch.Tensor = None,
+        x: torch.Tensor, # [B, T, C, H, W]
+        pos: torch.Tensor = None,  # [B, T, 2]
         repr_input: bool = False,
-        actions: torch.Tensor = None,
-        msgs: torch.Tensor = None,
+        actions: torch.Tensor = None,# [B, T, 1]
+        msgs: torch.Tensor = None,# [B, T, 32]
         T: int = None,
         goal: torch.Tensor = None
     ):
@@ -79,11 +79,11 @@ def forward(
     
     z0 = self.backbone(x, position=pos) if not repr_input else x
 
-    z0 = torch.einsum('b t c h w->t b c h w', z0)
-    actions = torch.einsum('b t d->t b d', actions)
-    msgs = torch.einsum('b t m->t b m', msgs)
+    z0 = torch.einsum('b t c h w->t b c h w', z0)# [T, B, C, H, W]
+    actions = torch.einsum('b t d->t b d', actions) # [T, B, D]
+    msgs = torch.einsum('b t m->t b m', msgs) # [T, B, M]
     
-    Z = self.dynamics.forward_multiple(z0, actions[:-1], msgs[:-1], T)
+    Z = self.dynamics.forward_multiple(z0, actions[:-1], msgs[:-1], T) # TODO: check if it should be msgs[1:]
 
     return z0, Z
 

mawm/models/misc.py
@@ -4,7 +4,7 @@
 
 # %% auto 0
 __all__ = ['PROBER_CONV_LAYERS_CONFIG', 'build_projector', 'build_norm1d', 'build_activation', 'PartialAffineLayerNorm',
-           'build_mlp', 'MLP', 'Prober', 'Projector']
+           'build_mlp', 'MLP', 'Prober', 'Projector', 'JepaProjector']
 
 # %% ../../nbs/02j_models.misc.ipynb 3
 from fastcore import *
@@ -231,3 +231,38 @@ class Projector(torch.nn.Module):
 
     def forward(self, x: torch.Tensor):
         return self.model(x)
+
+# %% ../../nbs/02j_models.misc.ipynb 5
+import torch
+from torch import nn
+from einops import rearrange
+class JepaProjector(nn.Module):
+    def __init__(self, z_input_dim=4050, c_input_dim=32):
+        super().__init__()
+        
+        self.z_projector = nn.Sequential(
+                nn.Linear(z_input_dim, 2048),
+                nn.BatchNorm1d(2048),
+                nn.ReLU(),
+                nn.Linear(2048, 128) # 128 is the 'Shared Latent Space'
+            )
+
+        self.msg_projector = nn.Sequential(
+            nn.Linear(c_input_dim, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Linear(2048, 128) # 128 is the 'Shared Latent Space'
+            )
+        
+    def forward(self, z_sender, C):
+        B, T, D = C.shape
+        z_sender = rearrange(z_sender, 'b t c h w -> (t b) (c h w)')
+        proj_z = self.z_projector(z_sender) # [(T*B, dim=128]
+        proj_z = rearrange(proj_z, '(t b) d -> t b d', b= B)
+
+        C = rearrange(rearrange(C, 'b t d -> t b d'), 't b d -> (t b) d')
+        proj_c = self.msg_projector(C) # [(T*B, dim=128]
+        proj_c = rearrange(proj_c, '(t b) d -> t b d', b= B)
+        
+        return  proj_z[:-1], proj_c[:-1]
+

mawm/models/vision/__init__.py
@@ -506,7 +506,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 class SemanticEncoder(nn.Module):
-    def __init__(self, num_primitives=5, latent_dim=128):
+    def __init__(self, num_primitives=5, latent_dim=32):
+        self.latent_dim = latent_dim
         super().__init__()
         self.net = nn.Sequential(
             nn.Conv2d(num_primitives, 16, kernel_size=3, padding=1),
@@ -523,5 +524,6 @@ class SemanticEncoder(nn.Module):
         B, T, C, H, W = x.shape
         x = rearrange(x, 'b t c h w -> (b t) c h w')
         x = self.net(x) # [B*T, latent_dim]
-        x = rearrange(x, '(b t) d -> t b d', b= B)
+        x = rearrange(x, '(b t) d -> b t d', b= B)
         return x
+

mawm/trainers/trainer_dynamics.py
@@ -31,7 +31,7 @@ class DynamicsTrainer(Trainer):
         self.val_loader = val_loader
 
         self.model = model['jepa']
-        self.msg_encoder = model['msg_enc']
+        self.msg_encoder = model['msg_encoder']
         self.projector = model['projector']
 
         self.optimizer = optimizer
@@ -55,29 +55,31 @@ class DynamicsTrainer(Trainer):
 
 # %% ../../nbs/05e_trainer.trainer_dynamics.ipynb 6
 from ..models.utils import flatten_conv_output
+from einops import rearrange
 @patch
 def train_epoch(self: DynamicsTrainer, epoch):
     self.model.train()
     
-    train_loss = 0
+    epoch_loss = 0
     actual_len = 0
 
     for batch_idx, data in enumerate(self.train_loader):
         self.optimizer.zero_grad()
+        batch_loss = 0
+        
 
         for agent_id in range(self.agents):
             obs, pos, _, act, _, dones = data[agent_id].values()
-            batch_samples = obs.size(0)
-            mask = ~dones.bool()     # keep only where done is False
+            mask = (~dones.bool()).float().to(self.device) # [B, T]
+            mask_t = rearrange(mask[:-1], 'b t -> t b') # [T-1, B]
+            
+            agent_loss = 0
+            actual_len = 0
 
             if mask.sum() == 0: # CHECK: mask is determined per the reciever agent
                 continue  # entire batch is terminals
 
-            obs = obs[mask]          # filter observations
-            pos = pos[mask]
-            msg = msg[mask]
-            act = act[mask]
-
+            batch_samples = mask.sum().item() / mask.size(1) # number of non-terminal samples in the batch
             obs = obs.to(self.device)
             pos = pos.to(self.device)
             msg = msg.to(self.device)
@@ -86,51 +88,53 @@ def train_epoch(self: DynamicsTrainer, epoch):
             for other_agent in range(self.agents):
                 if other_agent != agent_id:
                     obs_sender, pos_sender, msg, _, _, dones = data[other_agent].values()
-                    
-                    msg = msg[mask]
-                    obs_sender = obs_sender[mask]
-                    pos_sender = pos_sender[mask]
 
                     obs_sender = obs_sender.to(self.device)
                     pos_sender = pos_sender.to(self.device)
                     msg = msg.to(self.device)
-
             
-            C = self.msg_encoder(msg[:-1]) # [B, T-1, C, H, W] => [T-1, B, dim=32]
+            C = self.msg_encoder(msg) # [B, T, C, H, W] => [B, T, dim=32]
 
-            Z0, Z = self.model(x= obs, pos= pos, actions= act[:-1], msgs= C, T= act.size(0)-1)  # Z0, Z: [T-1, B, c, h, w]
-            vicreg_loss = self.vicreg(Z0, Z)
+            Z0, Z = self.model(x= obs, pos= pos, actions= act, msgs= C, T= act.size(1)-1)#[B, T, c, h, w] =>  [T, B, c, h, w]
+            vicreg_loss = self.vicreg(Z0, Z, mask= mask)
             
-            z_sender = self.model.backbone(obs_sender[:-1], position= pos_sender[:-1])  # [T-1, B, c, h, w]
-            proj_z, proj_c = self.projector(z_sender, C)
+            z_sender = self.model.backbone(obs_sender, position = pos_sender)  #[B, T, c, h, w] => [B, T, c`, h`, w`]
+            
+            proj_z, proj_c = self.projector(z_sender, C)# [B, T, *] => [T-1, B, dim=128]
+            
+            inv_loss = (proj_z - proj_c).square().mean(dim= -1)  # [T-1, B]
+            inv_loss = (inv_loss * mask[:-1]).sum() / mask[:-1].sum().clamp_min(1) 
+            
+            valid_idx = mask_t.bool()
+            sigreg_img = self.sigreg(proj_z[valid_idx])
+            sigreg_msg = self.sigreg(proj_c[valid_idx])
 
-            inv_loss = (proj_z - proj_c).square().mean()
-
-            sigreg_img = self.sigreg(proj_z)
-            sigreg_msg = self.sigreg(proj_c)
-
-            sigreg_loss = (sigreg_img + sigreg_msg ) / 2.0
+            sigreg_loss = 0.5 * (sigreg_img + sigreg_msg )
             lejepa_loss = (1- self.lambda_) * inv_loss + self.lambda_ * sigreg_loss 
 
-            loss = (lejepa_loss + vicreg_loss['total_loss']) / len(self.agents)
-            loss.backward()
+            agent_loss = lejepa_loss + vicreg_loss['total_loss']
+            batch_loss += agent_loss
+            
+            num_valid = mask[:, :-1].sum().item()
+            epoch_loss += agent_loss.item() * num_valid
+            actual_len += num_valid
 
-            train_loss += loss.item() * batch_samples            
-        
-        actual_len += batch_samples
+        loss = batch_loss / len(self.agents)
+        loss.backward()
         self.optimizer.step()
-               
+       
+
         if batch_idx % 20 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(obs), len(self.train_loader.dataset),
                 100. * batch_idx / len(self.train_loader),
                 loss.item() / len(obs)))
-        
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
             epoch, train_loss / actual_len))
 
-    return train_loss / actual_len
+    train_loss /= actual_len
+    return train_loss 
        
 
 # %% ../../nbs/05e_trainer.trainer_dynamics.ipynb 7
@@ -139,25 +143,24 @@ from einops import rearrange
 def eval_epoch(self: DynamicsTrainer):
     self.model.eval()
 
-    val_loss = 0
-    actual_len = 0
+    epoch_loss = 0
 
     with torch.no_grad():
-
-        for batch_idx, data in enumerate(self.val_loader):
+        for batch_idx, data in enumerate(self.train_loader):
+            batch_loss = 0
+            
             for agent_id in range(self.agents):
-
-                obs, pos, msg, act, _, dones = data[agent_id].values()
-                mask = ~dones.bool()     # keep only where done is False
-
-                if mask.sum() == 0:
+                obs, pos, _, act, _, dones = data[agent_id].values()
+                mask = (~dones.bool()).float().to(self.device) # [B, T]
+                mask_t = rearrange(mask[:-1], 'b t -> t b') # [T-1, B]
+                
+                agent_loss = 0
+                actual_len = 0
+                
+                if mask.sum() == 0: # CHECK: mask is determined per the reciever agent
                     continue  # entire batch is terminals
 
-                obs = obs[mask]          # filter observations
-                pos = pos[mask]
-                msg = msg[mask]
-                act = act[mask]
-
+                batch_samples = mask.sum().item() / mask.size(1) # number of non-terminal samples in the batch
                 obs = obs.to(self.device)
                 pos = pos.to(self.device)
                 msg = msg.to(self.device)
@@ -166,41 +169,41 @@ def eval_epoch(self: DynamicsTrainer):
                 for other_agent in range(self.agents):
                     if other_agent != agent_id:
                         obs_sender, pos_sender, msg, _, _, dones = data[other_agent].values()
-                        
-                        msg = msg[mask]
-                        obs_sender = obs_sender[mask]
-                        pos_sender = pos_sender[mask]
 
                         obs_sender = obs_sender.to(self.device)
                         pos_sender = pos_sender.to(self.device)
                         msg = msg.to(self.device)
-
-                C = self.msg_encoder(msg[:-1]) # [B, T-1, dim=32]
-                Z0, Z = self.model(x= obs, pos= pos, actions= act[:-1], msgs= C, T= self.cfg.data.seq_len - 1)
-                vicreg_loss = self.vicreg(Z0, Z)
                 
-                z_sender = self.model.backbone(obs_sender[:-1], position= pos_sender)
-                z_sender = rearrange(z_sender, 't b c h w -> (b t) (c h w)')
+                C = self.msg_encoder(msg) # [B, T, C, H, W] => [B, T, dim=32]
 
-                proj_z = self.z_projector(z_sender)
-                proj_c = self.msg_projector(C)
+                Z0, Z = self.model(x= obs, pos= pos, actions= act, msgs= C, T= act.size(1)-1)#[B, T, c, h, w] =>  [T, B, c, h, w]
+                vicreg_loss = self.vicreg(Z0, Z, mask= mask)
+                
+                z_sender = self.model.backbone(obs_sender, position = pos_sender)  #[B, T, c, h, w] => [B, T, c`, h`, w`]
+                
+                proj_z, proj_c = self.projector(z_sender, C)# [B, T, *] => [T-1, B, dim=128]
+                
+                inv_loss = (proj_z - proj_c).square().mean(dim= -1)  # [T-1, B]
+                inv_loss = (inv_loss * mask[:-1]).sum() / mask[:-1].sum().clamp_min(1) 
+                
+                valid_idx = mask_t.bool()
+                sigreg_img = self.sigreg(proj_z[valid_idx])
+                sigreg_msg = self.sigreg(proj_c[valid_idx])
 
-                inv_loss = (proj_z - proj_c).square().mean()
-
-                sigreg_img = self.sigreg(proj_z)
-                sigreg_msg = self.sigreg(proj_c)
-
-                sigreg_loss = (sigreg_img + sigreg_msg ) / 2.0
+                sigreg_loss = 0.5 * (sigreg_img + sigreg_msg )
                 lejepa_loss = (1- self.lambda_) * inv_loss + self.lambda_ * sigreg_loss 
 
-                loss = lejepa_loss + vicreg_loss['total_loss']
-                train_loss += loss.item() * obs.size(0)
-                val_loss += loss.item() * obs.size(0)
-                actual_len += obs.size(0)
+                agent_loss = lejepa_loss + vicreg_loss['total_loss']
+                batch_loss += agent_loss
+                
+                num_valid = mask[:, :-1].sum().item()
+                epoch_loss += agent_loss.item() * num_valid
+                actual_len += num_valid 
+                
 
-    val_loss /= actual_len
-    print('====> Test set loss: {:.4f}'.format(val_loss))
-    return val_loss
+    epoch_loss /= actual_len
+    print('====> Test set loss: {:.4f}'.format(epoch_loss))
+    return epoch_loss
 
 # %% ../../nbs/05e_trainer.trainer_dynamics.ipynb 8
 import wandb
@@ -209,8 +212,7 @@ import wandb
 def fit(self: DynamicsTrainer):
     self.model.to(self.device)
     self.msg_encoder.to(self.device)
-    self.z_projector.to(self.device)
-    self.msg_projector.to(self.device)
+    self.projector.to(self.device)
     
     cur_best = None
     lst_dfs = []
@@ -230,7 +232,10 @@ def fit(self: DynamicsTrainer):
 
         state = {
             'epoch': epoch,
-            'state_dict': self.model.state_dict(),
+            'jepa': self.model.state_dict(),
+            'msg_encoder': self.msg_encoder.state_dict(),
+            'projector': self.projector.state_dict(),
+            'train_loss': train_loss,
             'val_loss': val_loss,
             'optimizer': self.optimizer.state_dict(),
         }